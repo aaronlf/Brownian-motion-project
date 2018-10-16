d84134138ddd411ca8e09c9aca0ce28bd091d6ea
aaronlf <36794466+aaronlf@users.noreply.github.com>
2018-10-16 22:04:39 +0100

    Added rc.mc_mean and rw.mc_variance calls

--- random walk/example.py
+++ random walk/example.py
@@ -2,9 +2,13 @@ from randomwalk import RandomWalk
 
 rw = RandomWalk(p=0.6,n=100)
 
-print(rw.variance)                       #Get the variance of the random walk object
-print(rw.mean)                           #Get the mean of the random walk object
+print("Mean:",rw.mean)                           #Get the mean of the random walk object
+print("Variance:",rw.variance)                   #Get the variance of the random walk object
 
-rw.plot_distribution()                   #Plot the probability distribution
-print(rw.get_confidence_interval(-2,1))  #Get the probability that x_n is between -2 and 1
-rw.plot_monte_carlo(histogram=False)     #Run a Monte-Carlo simulation and plot results
+rw.plot_distribution()                           #Plot the probability distribution
+print(rw.get_confidence_interval(-2,1))          #Get the probability that x_n is between -2 and 1
+rw.plot_monte_carlo(number_of_trials=5000)       #Run a Monte-Carlo simulation and plot results
+
+print("Monte Carlo mean:",rw.mc_mean)            #Get the mean of the Monte Carlo results
+print("Monte Carlo variance:",rw.mc_variance)    #Get the variance of the Monte Carlo results
+
