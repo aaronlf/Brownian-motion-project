974267adc476717f9e8b85eb3798d6863a1daec1
aaronlf <36794466+aaronlf@users.noreply.github.com>
2018-10-18 14:09:12 +0100

    Simple bash script to fetch Python dependencies

---
+++ get_dependencies.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Execute this bash script to quickly get all relevant Python 3 dependencies
+
+sudo apt-get install python3
+sudo apt-get install python3-pip
+sudo apt-get install python3-tk
+
+pip3 install matplotlib
+pip3 install numpy
+pip3 install scipy
+pip3 install pandas
