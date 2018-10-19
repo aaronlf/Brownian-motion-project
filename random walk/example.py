e95f7a351f58e4c09df096b5a65be7adc8d053b7
aaronlf <36794466+aaronlf@users.noreply.github.com>
2018-10-19 17:12:35 +0100

    Update example.py

--- random walk/example.py
+++ random walk/example.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 from randomwalk import RandomWalk
 
 rw = RandomWalk(p=0.6,n=100)
@@ -6,9 +8,10 @@ print("Mean:",rw.mean)                           #Get the mean of the random wal
 print("Variance:",rw.variance)                   #Get the variance of the random walk object
 
 rw.plot_distribution()                           #Plot the probability distribution
-print(rw.get_confidence_interval(-2,1))          #Get the probability that x_n is between -2 and 1
-rw.plot_monte_carlo(number_of_trials=5000)       #Run a Monte-Carlo simulation and plot results
+print("Confidence interval probability:",
+	  rw.get_confidence_interval(-2,1))          #Get the probability that x_n is between -2 and 1
 
+rw.plot_monte_carlo(number_of_trials=5000)       #Run a Monte-Carlo simulation and plot results
 print("Monte Carlo mean:",rw.mc_mean)            #Get the mean of the Monte Carlo results
 print("Monte Carlo variance:",rw.mc_variance)    #Get the variance of the Monte Carlo results
 
