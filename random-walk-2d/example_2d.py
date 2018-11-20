6c8c01c95988ca8f649407055c7bc0f3d012cbe4
aaronlf <aaron.lalorfitzpatrick2@mail.dcu.ie>
2018-11-20 14:53:51 +0000

    Added 3D random walk class

--- random-walk-2d/example_2d.py
+++ random-walk-2d/example_2d.py
@@ -6,8 +6,13 @@ from randomwalk2d import RandomWalk2D
 
 
 # ~~~ CREATE 2D RANDOM WALK INSTANCE ~~~ #
-rw = RandomWalk2D(p=0.5,n=1000)
+rw = RandomWalk2D(n=200)
 
 
 # ~~~  DRAW REAL RANDOM WALK SIMULATION (STATIC GRAPH OR LIVE ANIMATION) ~~~ ##
-rw.random_walk_draw(5,animated=True)
+topview = rw.random_walk_draw(50,animated=True,projection='2d')
+view_3d = rw.random_walk_draw(50,animated=True,projection='3d')
+
+
+
+plt.show()
--- random-walk-2d/randomwalk2d.py
+++ random-walk-2d/randomwalk2d.py
@@ -6,6 +6,7 @@ from scipy import special
 import random
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.animation as anim
 from randomwalk import RandomWalk
 
@@ -19,14 +20,14 @@ class RandomWalk2D(RandomWalk):
 	
 	###################### CONSTRUCTOR ######################
 	
-	def __init__(self,p,n,delta_x=1,x_initial=0,stats=True,y_initial=0):
+	def __init__(self,n,delta_x=1,x_initial=0,y_initial=0,stats=False):
 		self.y_initial = y_initial
-		super().__init__(p,n,delta_x,x_initial,stats)
+		super().__init__(0.5,n,delta_x,x_initial,stats)
 	
 	
 	###################### USER-CALLABLE METHODS ######################
 	
-	def run_monte_carlo(self,number_of_trials=2000,plot=True,show=True):
+	def run_monte_carlo(self,number_of_trials=2000,plot=True,show=True): #NEEDS TO BE COMPLETELY CHANGED
 		"""Runs a Monte Carlo simulation of the random walk for a specified number of trials.
 		It then plots the results as a frequency distribution.
 		
@@ -36,7 +37,6 @@ class RandomWalk2D(RandomWalk):
 		#####  Parameters  #####
 		- number_of_trials: Integer number of Monte Carlo trials to be conducted. 2000 by default.
 		- plot: If true, plot the monte carlo trial results
-		- histogram: If true, plot a histogram of the Monte Carlo results
 		- show: If true, plt.show() will be called for each figure, one at a time.
 		- overlay: If true, the theoretical pdf will be plotted on top of the Monte Carlo trial
 		"""
@@ -75,7 +75,7 @@ class RandomWalk2D(RandomWalk):
 		return trial_data
 		
 		
-	def random_walk_draw(self,num_plots,animated=False,show=True):
+	def random_walk_draw(self,num_plots,animated=False,show=True,projection='2d'):
 		"""This method draws an animated 2D random walk simulation.
 		"""
 		x_y_arrays = []
@@ -85,44 +85,64 @@ class RandomWalk2D(RandomWalk):
 			x_array = [current_x]
 			y_array = [current_y]
 			x_steps,y_steps = self._random_walk_simulation()
-			for i in range(len(x_steps)):
+			for i in range(len(x_steps)-1):
 				current_x += x_steps[i]
 				current_y += y_steps[i]
 				x_array.append(current_x)
 				y_array.append(current_y)
 			x_y_arrays.append([x_array,y_array])
 				
-		fig = plt.figure('Random walk live simulation')
-		ax = fig.add_subplot(1,1,1)
+		fig = plt.figure('Random walk live simulation, {} trials, n = {}'.format(num_plots,self.n))
+		if projection == '3d':
+				ax=Axes3D(fig)
+				t_array = np.arange(len(x_steps))
+				ax.set_zlim([0,self.n])
+				extra_title_text = " (3D view)"
+		else:
+			ax = fig.add_subplot(1,1,1,)
+			t_array = None
+			extra_title_text = ""
 		ax.set_ylim([-np.sqrt(self.n)*2,np.sqrt(self.n)*2])
 		ax.set_xlim([-np.sqrt(self.n)*2,np.sqrt(self.n)*2])
 		
 		if animated == True:
-			fig.suptitle('Simulation of 2D random walk, live')
+			fig.suptitle('Simulation of 2D random walk, live' + extra_title_text)
 			self.index = 0
 			def update(i):
 				ax.clear()
 				ax.set_ylim([-np.sqrt(self.n)*2,np.sqrt(self.n)*2])
 				ax.set_xlim([-np.sqrt(self.n)*2,np.sqrt(self.n)*2])
+				if projection == '3d':
+					ax.set_zlim([0,self.n])
+
 				for i in x_y_arrays:
 					x_arr,y_arr = i
-					ax.plot(x_arr[:self.index], y_arr[:self.index])
+					if projection == '3d':
+						ax.plot(x_arr[:self.index], y_arr[:self.index],t_array[:self.index],linewidth=0.5)
+					else:
+						ax.plot(x_arr[:self.index], y_arr[:self.index],linewidth=0.5)
 				self.index += 1
 			a = anim.FuncAnimation(fig, update, frames=self.n, repeat=False,interval=10)
 		else:
-			fig.suptitle('Simulation of 2D random walk, static')
+			fig.suptitle('Simulation of 2D random walk, static' + extra_title_text)
 			for i in x_y_arrays:
 				x_vals,y_vals = i
-				ax.plot(x_vals, y_vals)
+				if projection == '3d':
+					ax.plot(x_vals, y_vals,t_array,linewidth=0.5)
+				else:
+					ax.plot(x_vals, y_vals,linewidth=0.5)
+		
+		plt.xlabel("x")
+		plt.ylabel("y")
 		
 		if show == True:
 			plt.show()
+		return fig
 		
 		
 	###################### PRIVATE METHODS ######################
 	
 	def _random_walk_simulation(self):
-		r = random.random() * 2 * np.pi
 		angles = [random.random()*2*np.pi for _ in range(self.n)]
 		x_coordinates = [np.cos(angle) for angle in angles]
 		y_coordinates = [np.sin(angle) for angle in angles]
