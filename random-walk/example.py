8d78f6898a3bc4c91b15660142c11022c3cbb194
aaronlf <aaron.lalorfitzpatrick2@mail.dcu.ie>
2018-11-20 12:58:37 +0000

    Cleaned up randomwalk class and example script.

--- random-walk/example.py
+++ random-walk/example.py
@@ -1,17 +1,45 @@
 #!/usr/bin/env python3
 
+import numpy as np
+import matplotlib.pyplot as plt
 from randomwalk import RandomWalk
 
-rw = RandomWalk(p=0.6,n=100)
 
-print("Mean:",rw.mean)                           #Get the mean of the random walk object
-print("Variance:",rw.variance)                   #Get the variance of the random walk object
+# ~~~ CREATE RANDOM WALK INSTANCE ~~~ #
+rw = RandomWalk(p=0.5,n=800) 					#Max n is ~800
 
-rw.plot_distribution()                           #Plot the probability distribution
-print("Confidence interval probability:",
-	  rw.get_confidence_interval(-2,1))          #Get the probability that x_n is between -2 and 1
 
-rw.plot_monte_carlo(number_of_trials=5000)       #Run a Monte-Carlo simulation and plot results
-print("Monte Carlo mean:",rw.mc_mean)            #Get the mean of the Monte Carlo results
-print("Monte Carlo variance:",rw.mc_variance)    #Get the variance of the Monte Carlo results
+# ~~~ PRINT RANDOM WALK STATS ~~~ #
+print("Mean:",rw.mean)                         	#Print the mean of the random walk object
+print("Variance:",rw.variance,'\n')            	#Print the variance of the random walk object
+print("Mean distance:",rw.mean_distance)		#Print theoretical mean distance from starting position
+print("Square root of n:",np.sqrt(rw.n),'\n')	#Print square root of n for comparison with mean distance
+
+
+# ~~~  PLOT THEORETICAL PROBABILITY DISTRIBUTION ~~~ #
+rw.plot_distribution(show=False)
+
+
+# ~~~ GET PROBABILITY THAT GIVEN X_VALUE IS WITHIN CONFIDENCE INTERVAL ~~~ #
+p_conf = rw.get_confidence_interval(-2,1)			#Get probability that x_n is between -2 and 1.
+print("Confidence interval probability:",p_conf)	#Print this probability
+
+
+# ~~~ RUN MONTE CARLO SIMULATION, PLOTTING RESULTS AND PRINTING STATS ~~~ #
+rw.run_monte_carlo(number_of_trials=10000,show=False,plot=True,histogram=True)
+print("Monte Carlo mean:",rw.mc_mean)            		#Print the mean of the M.C. results
+print("Monte Carlo variance:",rw.mc_variance,'\n') 		#Print the variance of the M.C. results
+print("Monte Carlo mean distance:",rw.mc_mean_distance) #Print the mean distance of the M.C. results
+print("Square root of n:",np.sqrt(rw.n))				#Print the square root of n for comparison with mean distance
+
+
+# ~~~  DRAW REAL RANDOM WALK SIMULATION (STATIC GRAPH OR LIVE ANIMATION) ~~~ #
+rw.random_walk_draw(10,animated=False,show=False)		#Plot simulation of 10 random walks on the same figure
+
+
+# ~~~ SHOW PLOTS ~~~ #
+"""Notice how each plotting method has the parameter "show=False". This is so they can be shown at 
+the end instead of individually.
+"""
+plt.show()
 
--- random-walk/randomwalk.py
+++ random-walk/randomwalk.py
@@ -3,6 +3,7 @@
 __author__ = "Aaron Lalor-Fitzpatrick"
 
 from scipy import special
+import random
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as anim
@@ -21,6 +22,8 @@ class RandomWalk:
 	  random walk. Two graphs can be plotted: A histogram (optional) with N on the y-axis, and/or
 	  a normalised version of this (all N-values divided by n) that resembles a pdf.
 	"""
+	
+	###################### CONSTRUCTOR ######################
 	
 	def __init__(self,p,n,delta_x=1,x_initial=0,stats=True):
 		
@@ -48,6 +51,9 @@ class RandomWalk:
 			self.tuple_of_probabilities = self._get_tuple_of_probabilities()
 			self.mean_distance = self._calculate_mean_distance_theoretical()
 		
+		
+	###################### USER-CALLABLE METHODS ######################
+	
 	def get_confidence_interval(self,a,b):
 		"""Returns the theoretical probability that x_n will be between the values a and b.
 		"""
@@ -56,25 +62,38 @@ class RandomWalk:
 		working_prob_vals = [prob_vals[i] for i in working_indices]
 		return sum(working_prob_vals)
 		
-	def plot_distribution(self):
+		
+	def plot_distribution(self,show=True):
 		"""Plots the theoretical probability distribution for the random walk.
 		"""
 		k_vals,prob_vals = self.tuple_of_probabilities
 		
-		plt.figure("Probability Distribution of Random Walk, Theoretical")
+		plt.figure("Probability distribution of Random Walk, theoretical")
 		plt.scatter(k_vals,prob_vals,s=4)
 		plt.xlim((-self.n-1,self.n+1))
 		
 		plt.xlabel("x\u2099 - Position after n jumps")
 		plt.ylabel("Probability")
 		plt.suptitle("Random Walk: p={}, n={}, \u0394x={}".format(self.p,self.n,self.delta_x))
+		if show == True:
+			plt.show()
 
-	def plot_monte_carlo(self,number_of_trials=2000,histogram=False,show=True): # Deal with histogram later
+
+	def run_monte_carlo(self,number_of_trials=2000,plot=True,histogram=False,show=True,overlay=False):
 		"""Runs a Monte Carlo simulation of the random walk for a specified number of trials.
 		It then plots the results as a frequency distribution.
 		
-		Mean and variance values of the Monte Carlo simulation can now be retrieved.
+		Mean and variance values of the Monte Carlo simulation can be retrieved by calling mc.mean
+		and mc.variance, respectively.
+		
+		#####  Method parameters  #####
+		- number_of_trials: Integer number of Monte Carlo trials to be conducted. 2000 by default.
+		- plot: If true, plot the monte carlo trial results
+		- histogram: If true, plot a histogram of the Monte Carlo results
+		- show: If true, plt.show() will be called for each figure, one at a time.
+		- overlay: If true, the theoretical pdf will be plotted on top of the Monte Carlo trial
 		"""
+		
 		trial_data = []
 		for _ in range(number_of_trials):
 			steps = self._random_walk_simulation()
@@ -91,74 +110,41 @@ class RandomWalk:
 			mean_total += weighted_distance
 		self.mc_mean_distance = mean_total/number_of_trials
 		
-		if show == False:
+		if histogram == True:
+			plt.figure("Monte Carlo simulation of random walk - results")
+			plt.hist( trial_data, bins=int(round(np.sqrt(self.n))) )
+			plt.suptitle("Histogram of Monte Carlo simulation results: p={},n={}, \u0394x={}, N={}".format(
+													self.p,self.n,self.delta_x,number_of_trials))
+			if show == True:
+				plt.show()
+			
+		
+		if plot == False:
 			return trial_data
 			
-		plt.figure("Monte Carlo Simulation of Random Walk")
+		plt.figure("Monte Carlo simulation of Random Walk")
 		plt.scatter(x_n,counts,s=4)
 		plt.xlim((-self.n-1,self.n+1))
-		
 		plt.xlabel("x\u2099 - Position after n jumps")
 		plt.ylabel("Frequency")
-		plt.suptitle("Monte Carlo Simulation of Random Walk: p={}, n={}, \u0394x={}, N={}".format(
+		plt.suptitle("Monte Carlo simulation of random walk: p={}, n={}, \u0394x={}, N={}".format(
 													self.p,self.n,self.delta_x,number_of_trials))
+		
+		if overlay == True: # IF TRUE, PLOT THEORETICAL RESULTS OVER MONTE CARLO RESULTS
+			k_vals,prob_vals = self.tuple_of_probabilities
+			prob_vals = [p*number_of_trials for p in prob_vals]
+			plt.scatter(k_vals,prob_vals,s=4)	
+		
+		if show == True:
+			plt.show()		
+								
 		return trial_data
-		
-	def factorial(self,num):
-		# Gosper approximation was meant to fix num > 1000 case but this number computes to 0.0
-		# = np.sqrt((2*num+(1/3))*np.pi) * (num**num) * (np.e**(-num))
-		res = 1
-		for i in range(1,int(num+1)):
-			res += i
-		return res 
-			
-	def _calculate_binom(self,n,r):
-		#From formula: nCr = n! / r!(n-r)!
-		return self.factorial(n) / ( self.factorial(r) * self.factorial(n-r) )
-			
-	def _calculate_mean(self):
-		return (self.p - (1-self.p)) * self.n * self.delta_x
-		
-	def _calculate_variance(self):
-		return 4 * self.p * (1-self.p) * self.n * (self.delta_x)**2
-		
-	def _calculate_probability(self,k):
-		"""Calculates the probability that x_n = k * delta_x.
-		
-		This method uses the values of n and p in its calculations.
-		"""
-		if abs(k * self.delta_x) > (3 * np.sqrt(self.variance)):
-			return 0.0
-		binom_coeff = special.binom(self.n,(self.n + k)/2)
-		b_value = binom_coeff * ((self.p) ** ((self.n + k)/2)) * ((1-self.p) ** ((self.n - k)/2))
-		return b_value
-		
-	def _get_tuple_of_probabilities(self):
-		"""Gets a tuple of the form (k-values,probabilities) in the range [-n,n].
-		"""
-		k_array = np.arange(-self.n,self.n+1,2)
-		probability_array = []
-		
-		for k in k_array:
-			probability_array.append(self._calculate_probability(k))
-			
-		return (k_array,probability_array)
-		
-	def _calculate_mean_distance_theoretical(self):
-		x_mean_distance = 0
-		x_vals,prob_vals = self.tuple_of_probabilities
-		for i in range(len(x_vals)):
-			x_val, prob = x_vals[i], prob_vals[i]
-			x_distance = abs(x_val - self.x_initial)
-			x_weighted = x_distance * prob
-			x_mean_distance += x_weighted
-		return x_mean_distance
-		
-	def _random_walk_simulation(self):
-		steps = np.random.choice( [-1,1], self.n, p = [1-self.p,self.p] )
-		return steps
 
-	def random_walk_draw(self,num_plots):
+	
+	def random_walk_draw(self,num_plots,animated=False,show=True):
+		"""This method produces an animated simulation of a 1D random walk.
+		"""
+		
 		t_x_arrays = []
 		t_max = self.n
 		for _ in range(num_plots):
@@ -172,22 +158,101 @@ class RandomWalk:
 			t_x_arrays.append( [x_array,t_array] )
 		
 		
-		fig = plt.figure('Random walk live simulation')
+		fig = plt.figure('Random walk simulation')
 		ax = fig.add_subplot(1,1,1)
-
-		self.index = 0
-		def update(i):
-			ax.set_ylim([-self.n,self.n])
-			ax.set_xlim([0,self.n])
+		ax.set_ylim([(round(min(x_array) - np.sqrt(self.n)*3)),round(max(x_array) + np.sqrt(self.n)*3)])
+		ax.set_xlim([-(round(np.sqrt(self.n))),self.n+(round(np.sqrt(self.n)))])
+		
+		if animated == True: # THIS CASE CURRENTLY HAS BUG FOR SOME REASON. CODE IS IDENTICAL TO 2D ANIMATION?
+			fig.suptitle('Simulation of 1D random walk, live')
+			self.index = 0
+			def update(i):
+				ax.clear()
+				ax.set_ylim([(round(min(x_array) - np.sqrt(self.n)*3)), round(max(x_array) + np.sqrt(self.n)*3)])
+				ax.set_xlim([-(round(np.sqrt(self.n))), self.n+(round(np.sqrt(self.n)))])
+				for i in t_x_arrays:
+					x_vals,t_vals = i 
+					ax.plot(t_vals[:self.index], x_vals[:self.index])
+				self.index += 1
+			a = anim.FuncAnimation(fig, update, frames=self.n, repeat=False,interval=10)
+		else:
+			fig.suptitle('Simulation of 1D random walk, static')
 			for i in t_x_arrays:
 				x_vals,t_vals = i
-				ax.plot(t_vals[:self.index], x_vals[:self.index])
-			self.index += 1
-		a = anim.FuncAnimation(fig, update, frames=t_max+1, repeat=False)
-		plt.show()
+				ax.plot(t_vals, x_vals)
+			
+		if show == True:
+			plt.show()
+
+			
+	###################### PRIVATE METHODS ######################
+			
+	def _calculate_binom(self,n,r):
+		#From formula: nCr = n! / r!(n-r)!
+		return self._factorial(n) / ( self._factorial(r) * self._factorial(n-r) )
+
+			
+	def _calculate_mean(self):
+		return (self.p - (1-self.p)) * self.n * self.delta_x
+		
+	
+	def _calculate_mean_distance_theoretical(self):
+		"""Returns the theoretical average distance from x_initial.
+		"""
+		x_mean_distance = 0
+		x_vals,prob_vals = self.tuple_of_probabilities
+		for i in range(len(x_vals)):
+			x_val, prob = x_vals[i], prob_vals[i]
+			x_distance = abs(x_val - self.x_initial)
+			x_weighted = x_distance * prob
+			x_mean_distance += x_weighted
+		return x_mean_distance
+		
+		
+	def _calculate_probability(self,k):
+		"""Calculates the probability that x_n = k * delta_x.
+		
+		This method uses the values of n and p in its calculations.
+		"""
+		if abs(k * self.delta_x) > (3 * np.sqrt(self.variance)):
+			return 0.0
+		binom_coeff = special.binom(self.n,(self.n + k)/2)
+		b_value = binom_coeff * ((self.p) ** ((self.n + k)/2)) * ((1-self.p) ** ((self.n - k)/2))
+		return b_value
+		
+			
+	def _calculate_variance(self):
+		return 4 * self.p * (1-self.p) * self.n * (self.delta_x)**2
+
+	
+	def _factorial(self,num):
+		res = 1
+		for i in range(1,int(num+1)):
+			res += i
+		return res 
+		
+			
+	def _get_tuple_of_probabilities(self):
+		"""Gets a tuple of the form (k-values,probabilities) in the range [-n,n].
+		"""
+		k_array = np.arange(-self.n,self.n+1,2)
+		probability_array = []
+		
+		for k in k_array:
+			probability_array.append(self._calculate_probability(k))
+			
+		return (k_array,probability_array)
+		
+
+	def _random_walk_simulation(self):
+		steps = np.random.choice( [-1,1], self.n, p = [1-self.p,self.p] )
+		return steps
+
+	
+	###################### OPERATOR OVERLOADING ######################
 		
 	def __getattr__(self,name):
 		if name in ['mc_mean','mc_variance']:
 			print("You must run a Monte Carlo simulation first before calling '{}'".format(name))
 		raise AttributeError
-		
+
