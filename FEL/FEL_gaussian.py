dc3a79636982f51c69b8ad4f49b89a4781cde886
aaronlf <aaron.lalorfitzpatrick2@mail.dcu.ie>
2019-03-01 00:45:08 +0000

    Autocorrelation functions added

--- FEL/FEL_gaussian.py
+++ FEL/FEL_gaussian.py
@@ -7,6 +7,7 @@ import time
 import numpy as np
 import pickle
 import matplotlib.pyplot as plt
+from bisect import bisect_left
 
 # Get current size
 fig_size = plt.rcParams["figure.figsize"]
@@ -22,13 +23,16 @@ plt.rcParams["figure.figsize"] = fig_size
 #---------------------------------------------------------------------------------------------------
 
 def main():
-	SHOW_COHERENT_RADIATION()
+	#SHOW_COHERENT_RADIATION()
 	#SHOW_INCOHERENT_RADIATION(100,subplots=True,plot_superposition=True,plot_intensity=True)
 	#INCOHERENT_AVERAGE_INTENSITY_VS_NUM_SOURCES()
 	#SHOW_INCOHERENT_RADIATION_OMEGA(100,plot_superposition=True,plot_intensity=True)
 	#MONTE_CARLO_INTENSITY_TIME(num_trials=500,num_pulses=100,bunch_length=100,show_result=True)
 	#MONTE_CARLO_INTENSITY_OMEGA(num_trials=500,num_pulses=100,bunch_length=100,show_result=True)
-	
+	SHOW_AUTOCORRELATION_TIME(t=0,num_pulses=100,bunch_length=100,plot_e=True,plot_intensity=True)
+	MONTE_CARLO_AUTOCORRELATION_TIME(num_trials=500,t=0,num_pulses=100,bunch_length=100,show_result=True)
+	SHOW_AUTOCORRELATION_FREQUENCY(delta_omega=0,num_pulses=100,bunch_length=100,plot_e=True,plot_intensity=True)
+	MONTE_CARLO_AUTOCORRELATION_FREQUENCY(num_trials=500,delta_omega=0,num_pulses=100,bunch_length=100,show_result=True)
 	
 #---------------------------------------------------------------------------------------------------
 ### 					CLASS TO DEMONSTRATE TEMPORAL COHERENCE/INCOHERECE 						 ###
@@ -53,7 +57,7 @@ class Temporal:
 		for t in time_vals:
 			e_vals.append(self.coherent_rad(t,sigma,omega))
 		return {'x_vals':time_vals,'y_vals':e_vals,'label':'\u03C3 = {}, \u03C9 = {}'.format(sigma,omega)}
-
+		
 	def incoherent_rad(self,t,tj,sigma,omega,bunch_length):
 		return (np.exp((-(t-tj)**2)/(4*(sigma**2)))) * np.cos(omega*(t-tj))
 	
@@ -112,7 +116,63 @@ class Temporal:
 		delta_omega_vals = omega_vals - omega1
 		return {'x_vals':delta_omega_vals,'y_vals':e_vals,'label':'\u03C3 = {}, \u03C9$_1 = {}'.format(sigma,omega1)}
 		
-						
+			
+	def autocorrelation_time(self,t,num_pulses,bunch_length,sigma=None,omega=None):
+		if sigma == None:
+			sigma = self.sigma
+		if omega == None:
+			omega = self.omega
+			
+		incoherent_vals_list = [
+			self.model_incoherent_rad_once(bunch_length,sigma=None,omega=None) for i in range(num_pulses)
+			]
+		superposition_pulses = self.get_superposition(incoherent_vals_list)
+		time_vals = superposition_pulses['x_vals']
+		E = superposition_pulses['y_vals'][bisect_left(time_vals, t)]
+		E_conj = np.conjugate(E)
+		
+		c1_vals = []	# 1st order AC function
+		c2_vals = []	# 2nd order AC function
+		tau_vals = []
+		for i,time_val in enumerate(time_vals):
+			E_tau = superposition_pulses['y_vals'][i]
+			E_tau_conj = np.conjugate(E_tau)
+			tau_vals.append(time_val - t)
+			c1_vals.append( E * E_tau_conj )
+			c2_vals.append( E * E_conj * E_tau * E_tau_conj )
+		c1_dict = {'x_vals':tau_vals,'y_vals':c1_vals,'label':None}
+		c2_dict = {'x_vals':tau_vals,'y_vals':c2_vals,'label':None}
+		return {'c1_dict':c1_dict,'c2_dict':c2_dict}
+		
+		
+	def autocorrelation_freq(self,delta_omega,num_pulses,bunch_length,sigma=None,omega1=None):
+		if sigma == None:
+			sigma = self.sigma
+		if omega1 == None:
+			omega1 = self.omega
+			
+		incoherent_vals_list = [
+			self.model_incoherent_rad_once_omega(bunch_length,sigma=None,omega1=None) for i in range(num_pulses)
+			]
+		superposition_pulses = self.get_superposition(incoherent_vals_list)
+		delta_omega_vals = superposition_pulses['x_vals']
+		E = superposition_pulses['y_vals'][bisect_left(delta_omega_vals, delta_omega)]
+		E_conj = np.conjugate(E)
+		
+		c1_vals = []	# 1st order AC function
+		c2_vals = []	# 2nd order AC function
+		tau_freq_vals = []
+		for i,delta_omega_val in enumerate(delta_omega_vals):
+			E_tau = superposition_pulses['y_vals'][i]
+			E_tau_conj = np.conjugate(E_tau)
+			tau_freq_vals.append(delta_omega_val - delta_omega)
+			c1_vals.append( E * E_tau_conj )
+			c2_vals.append( E * E_conj * E_tau * E_tau_conj )
+		c1_dict = {'x_vals':delta_omega_vals,'y_vals':c1_vals,'label':None}
+		c2_dict = {'x_vals':delta_omega_vals,'y_vals':c2_vals,'label':None}
+		return {'c1_dict':c1_dict,'c2_dict':c2_dict}
+	
+
 #---------------------------------------------------------------------------------------------------
 ### 								PLOTTING FUNCTIONS											 ###
 #---------------------------------------------------------------------------------------------------
@@ -223,7 +283,7 @@ def MONTE_CARLO_INTENSITY_TIME(num_trials,num_pulses=100,bunch_length=100,show_r
 			intensity_vals = np.zeros(shape=len(intensity['y_vals']))
 			time_vals = intensity['x_vals']
 		intensity_vals += (intensity['y_vals'])
-	average_intensity_vals = intensity_vals / 50
+	average_intensity_vals = intensity_vals / num_trials
 	plot_dict = {
 				'x_vals':time_vals,
 				'y_vals':average_intensity_vals,
@@ -267,7 +327,7 @@ def MONTE_CARLO_INTENSITY_OMEGA(num_trials,num_pulses=100,bunch_length=100,show_
 			intensity_vals = np.zeros(shape=len(intensity['y_vals']))
 			time_vals = intensity['x_vals']
 		intensity_vals += (intensity['y_vals'])
-	average_intensity_vals = intensity_vals / 50
+	average_intensity_vals = intensity_vals / num_trials
 	plot_dict = {
 				'x_vals':time_vals,
 				'y_vals':average_intensity_vals,
@@ -282,6 +342,123 @@ def MONTE_CARLO_INTENSITY_OMEGA(num_trials,num_pulses=100,bunch_length=100,show_
 					show=show_result,scatter=False)	
 		
 
+def SHOW_AUTOCORRELATION_TIME(t=0,num_pulses=100,bunch_length=100,plot_e=True,plot_intensity=True):
+	
+	autocorrelation_dicts = temp.autocorrelation_time(t,num_pulses,bunch_length)
+	c1_data = autocorrelation_dicts['c1_dict']
+	c2_data = autocorrelation_dicts['c2_dict']
+	
+	if plot_e == True:
+		plot_graph([c1_data],'Autocorrelation function, E-field',
+					'Autocorrelation Function: E-Field in Time Domain',
+					'\u03C4','$C_1$(\u03C4)',f'E_autocorrelation_{num_pulses}_pulses_t={t}')
+
+	if plot_intensity == True:
+		plot_graph([c2_data],'Autocorrelation function, Intensity',
+					'Autocorrelation Function: Intensity in Time Domain',
+					'\u03C4','$C_2$(\u03C4)',f'I_autocorrelation_{num_pulses}_pulses_t={t}')
+	
+	
+
+def MONTE_CARLO_AUTOCORRELATION_TIME(num_trials,t=0,num_pulses=100,bunch_length=100,show_result=True):
+	for i in range(num_trials):
+		print('Trial number: {}/{}'.format(i+1,num_trials))
+		autocorrelation_dicts = temp.autocorrelation_time(t,num_pulses,bunch_length)
+		c1_data = autocorrelation_dicts['c1_dict']
+		c2_data = autocorrelation_dicts['c2_dict']
+		if i == 0:
+			c1_vals = np.zeros(shape=len(c1_data['y_vals']))
+			c2_vals = np.zeros(shape=len(c2_data['y_vals']))
+			tau_vals = c1_data['x_vals']
+		c1_vals += (c1_data['y_vals'])
+		c2_vals += (c2_data['y_vals'])
+		
+	average_c1_vals = c1_vals / num_trials
+	average_c2_vals = c2_vals / num_trials
+	c1_plot_dict = {
+				'x_vals':tau_vals,
+				'y_vals':average_c1_vals,
+				'label':None
+				}
+	c2_plot_dict = {
+				'x_vals':tau_vals,
+				'y_vals':average_c2_vals,
+				'label':None
+				}
+	with open(f'autocorrelation_c1_monte_carlo_{num_trials}_trials.pickle', 'wb') as f:
+			pickle.dump(c1_plot_dict,f)
+	with open(f'autocorrelation_c2_monte_carlo_{num_trials}_trials.pickle', 'wb') as f:
+			pickle.dump(c2_plot_dict,f)
+			
+	plot_graph([c1_plot_dict],'Monte Carlo Trialed $C_1$ in Time Domain',
+					'First-Order Autocorrelation vs Time',
+					'\u03C4','simulated average $C_1$(\u03C4)',
+					f'C1_monte_carlo_time_{num_trials}_trials',
+					show=show_result,scatter=False)
+	plot_graph([c2_plot_dict],'Monte Carlo Trialed $C_2$ in Time Domain',
+					'First-Order Autocorrelation vs Time',
+					'\u03C4','simulated average $C_2$(\u03C4)',
+					f'C2_monte_carlo_time_{num_trials}_trials',
+					show=show_result,scatter=False)
+					
+
+def SHOW_AUTOCORRELATION_FREQUENCY(delta_omega=0,num_pulses=100,bunch_length=100,plot_e=True,plot_intensity=True):
+	autocorrelation_dicts = temp.autocorrelation_freq(delta_omega,num_pulses,bunch_length)
+	c1_data = autocorrelation_dicts['c1_dict']
+	c2_data = autocorrelation_dicts['c2_dict']
+	
+	if plot_e == True:
+		plot_graph([c1_data],'Autocorrelation function, E-field',
+					'Autocorrelation Function: E-Field in Frequency Domain',
+					'Variation in \u03C9','$C_1$',f'E_autocorrelation_{num_pulses}_pulses_delta_omega={delta_omega}')
+
+	if plot_intensity == True:
+		plot_graph([c2_data],'Autocorrelation function, Intensity',
+					'Autocorrelation Function: Intensity in Frequency Domain',
+					'Variation in \u03C9','$C_2$',f'I_autocorrelation_{num_pulses}_pulses_delta_omega={delta_omega}')	
+
+
+def MONTE_CARLO_AUTOCORRELATION_FREQUENCY(num_trials,delta_omega=0,num_pulses=100,bunch_length=100,show_result=True):
+	for i in range(num_trials):
+		print('Trial number: {}/{}'.format(i+1,num_trials))
+		autocorrelation_dicts = temp.autocorrelation_freq(delta_omega,num_pulses,bunch_length)
+		c1_data = autocorrelation_dicts['c1_dict']
+		c2_data = autocorrelation_dicts['c2_dict']
+		if i == 0:
+			c1_vals = np.zeros(shape=len(c1_data['y_vals']))
+			c2_vals = np.zeros(shape=len(c2_data['y_vals']))
+			d_omega_vals = c1_data['x_vals']
+		c1_vals += (c1_data['y_vals'])
+		c2_vals += (c2_data['y_vals'])
+		
+	average_c1_vals = c1_vals / num_trials
+	average_c2_vals = c2_vals / num_trials
+	c1_plot_dict = {
+				'x_vals':d_omega_vals,
+				'y_vals':average_c1_vals,
+				'label':None
+				}
+	c2_plot_dict = {
+				'x_vals':d_omega_vals,
+				'y_vals':average_c2_vals,
+				'label':None
+				}
+	with open(f'freq_autocorrelation_c1_monte_carlo_{num_trials}_trials.pickle', 'wb') as f:
+			pickle.dump(c1_plot_dict,f)
+	with open(f'freq_autocorrelation_c2_monte_carlo_{num_trials}_trials.pickle', 'wb') as f:
+			pickle.dump(c2_plot_dict,f)
+			
+	plot_graph([c1_plot_dict],'Monte Carlo Trialed $C_1$ in Frequency Domain',
+					'First-Order Autocorrelation vs Change in Frequency',
+					'Variation in \u03C9','simulated average $C_1$',
+					f'C1_monte_carlo_freq_{num_trials}_trials',
+					show=show_result,scatter=False)
+	plot_graph([c2_plot_dict],'Monte Carlo Trialed $C_2$ in Frequency Domain',
+					'First-Order Autocorrelation vs Change in Frequency',
+					'Variation in \u03C9','simulated average $C_2$',
+					f'C2_monte_carlo_freq_{num_trials}_trials',
+					show=show_result,scatter=False)					
+									
 #---------------------------------------------------------------------------------------------------
 	
 	
